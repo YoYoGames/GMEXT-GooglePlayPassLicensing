6d9354627547613fedfc3aec2d2cb7698dd9d159
Francisco Dias <fdias@opera.com>
2024-11-18 12:47:02 +0000

    Updated "scriptUtils" files

--- source/GooglePlayPassLicensing_gml/extensions/GooglePlayPassLicensing/scriptUtils.sh
+++ source/GooglePlayPassLicensing_gml/extensions/GooglePlayPassLicensing/scriptUtils.sh
@@ -12,8 +12,9 @@ scriptInit() {
     EXTENSION_NAME=
 
     # Get extension data
-    pathExtractBase $SCRIPT_PATH EXTENSION_NAME
+    pathExtractBase "$SCRIPT_PATH" EXTENSION_NAME
     extensionGetVersion EXTENSION_VERSION
+
     if [ -z "$EXTENSION_VERSION" ]; then
         EXTENSION_VERSION="0.0.0"
     fi
@@ -43,13 +44,12 @@ extensionGetVersion() {
     set +f
 
     logInformation "Accessed extension version with value '${result}'."
-    eval "$1=\"\$result\""
+    printf -v "$1" "%s" "$result"
 }
 
 # Gets an extension option value
 # Usage: optionGetValue optionName result
 optionGetValue() {
-
     # Enable indirect variable reference
     set -f
     local var="YYEXTOPT_${EXTENSION_NAME}_$1"
@@ -57,26 +57,34 @@ optionGetValue() {
     set +f
 
     logInformation "Accessed extension option '${1}' with value '${result}'."
-    eval "$2=\"\$result\""
+    printf -v "$2" "%s" "$result"
 }
 
 # Sets a string to uppercase
 toUpper() { # str result
-    eval "$2=$(echo $1 | tr '[:lower:]' '[:upper:]')"
+    local _result
+    _result=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+    printf -v "$2" "%s" "$_result"
     logInformation "Converted string '$1' to upper case."
 }
+
 
 # Extracts the full folder path from a filepath
 # Usage: pathExtractDirectory fullpath result
 pathExtractDirectory() {
-    eval "$2=\"$(dirname "$1")\""
+    local _result
+    _result="$(dirname "$1")"
+    printf -v "$2" "%s" "$_result"
     logInformation "Extracted directory path from '$1'."
 }
+
 
 # Extracts the parent folder from a path
 # Usage: pathExtractBase fullpath result
 pathExtractBase() {
-    eval "$2=\"$(basename $(dirname "$1"))\""
+    local _result
+    _result="$(basename "$(dirname "$1")")"
+    printf -v "$2" "%s" "$_result"
     logInformation "Extracted base name from '$1'."
 }
 
@@ -86,6 +94,8 @@ pathResolve() {
     local basePath="$1"
     local relativePath="$2"
     local resolvedPath=
+    local combined_path
+    local result=()
 
     # Ensure 'basePath' ends with a forward slash
     [[ "${basePath: -1}" != "/" ]] && basePath+="/"
@@ -102,7 +112,6 @@ pathResolve() {
     IFS="/" read -ra path_parts <<< "$combined_path"
 
     # Remove any entries that are "." and if an entry is "..", remove that entry and the previous one
-    result=()
     for part in "${path_parts[@]}"; do
         if [ "$part" == "." ] || [ -z "$part" ]; then
             continue
@@ -124,8 +133,9 @@ pathResolve() {
 
     # Return the merged result
     logInformation "Resolved path into '$resolvedPath'."
-    eval "$3=\"$resolvedPath\""
+    printf -v "$3" "%s" "$resolvedPath"
 }
+
 
 # Resolves an existing relative path if required (handles errors)
 # Usage: pathResolveExisting basePath relativePath result
@@ -141,7 +151,7 @@ pathResolveExisting() {
     eval "$3=\"$existingPath\""
 }
 
-# Copies a file or folder to the specified destination folder (displays log messages)
+# Copies a file or folder to the specified destination folder
 # Usage: itemCopyTo srcPath destFolder
 itemCopyTo() {
     local source="$1"
@@ -151,50 +161,32 @@ itemCopyTo() {
     # Resolve the destination folder to an absolute path
     pathResolve "$PWD" "$destination" resolved_destination
 
-    if [[ ! -e "$source" ]]; then
-        logError "Failed to copy '$source' to '$resolved_destination' (source doesn't exist)."
+    # If 'resolved_destination' ends with a "/", ensure the path exists
+    if [[ "${resolved_destination: -1}" == "/" ]]; then
+        mkdir -p "$resolved_destination"
+    else
+        # Create all parent directories up until the destination path
+        parent_directory=$(dirname "$resolved_destination")
+        mkdir -p "$parent_directory"
+    fi
+
+    if [ -d "$source" ]; then
+        # Source is a folder
+        cp -rf "$source" "$resolved_destination"
+    elif [ -f "$source" ]; then
+        # Source is a file
+        cp -f "$source" "$resolved_destination"
+    else
+        logError "Failed to copy '$source' does not exist or is not accessible."
         exit 1
     fi
 
-    if [[ -d "$source" ]]; then
-        # Source is a folder
-        copyFolderContents "$source" "$resolved_destination"
-    elif [[ -f "$source" ]]; then
-        # Source is a file
-        copyFileToDestination "$source" "$resolved_destination"
-    fi
-
-    if [[ $? -ne 0 ]]; then
+    if [ $? -ne 0 ]; then
         logError "Failed to copy '$source' to '$resolved_destination'."
         exit 1
     fi
 
     logInformation "Copied '$source' to '$resolved_destination'."
-}
-
-# Copies a folder contents to the specified destination folder
-copyFolderContents() {
-    local src_folder="$1"
-    local dest_folder="$2"
-
-    mkdir -p "$dest_folder"
-    cp -R "$src_folder/"* "$dest_folder/"
-}
-
-# Copies a file specified destination path
-copyFileToDestination() {
-    local src_file="$1"
-    local dest_path="$2"
-
-    if [[ -d "$dest_path" || "$dest_path" =~ /$ || "$dest_path" =~ \/\.$ ]]; then
-        # Destination is a folder
-        mkdir -p "$dest_path"
-        cp "$src_file" "$dest_path/"
-    else
-        dest_dir="$(dirname "$dest_path")"
-        mkdir -p "$dest_dir"
-        cp "$src_file" "$dest_path"
-    fi
 }
 
 # Deletes a file or folder given a path
@@ -269,16 +261,57 @@ fileExtract() {
 # Compresses the contents of a folder into a zip file
 # Usage: folderCompress srcFolder destFile
 folderCompress() {
-    local source="$1"
-    local destination="$2"
+    local source=$1
+    local destination=$2
+    
+    # Make sure destination exists
+    touch "$destination"
 
-    # Compress the contents of the folder to the destination file
-    zip -j -r -q "$destination" "$source"
-
+    # Get the absolute path of the destination zip
+    local abs_destination=$(readlink -f "$destination")
+    
+    # Change directory to the folder
+    cd "$source" || logError "Source folder doesn't exist ''"
+    
+    # Compress the contents of the folder into the destination zip
+    zip -r -q "$abs_destination" *
+    
     if [ $? -ne 0 ]; then
         logError "Failed to compress contents of '$source' into '$destination'."
         exit 1
     fi
+
+    # Change back to the original directory
+    cd - >/dev/null 2>&1
+
+    logInformation "Compressed contents of '$source' into '$destination'."
+}
+
+# Adds the contents of a folder into a zip file
+# Usage: zipUpdate srcFolder destFile
+zipUpdate() {
+    local source=$1
+    local destination=$2
+    
+    # Make sure destination exists
+    touch "$destination"
+
+    # Get the absolute path of the destination zip
+    local abs_destination=$(readlink -f "$destination")
+    
+    # Change directory to the folder
+    cd "$source" || logError "Source folder doesn't exist ''"
+    
+    # Update the contents of the folder into the destination zip
+    zip -ur -q "$abs_destination" *
+    
+    if [ $? -ne 0 ]; then
+        logError "Failed to compress contents of '$source' into '$destination'."
+        exit 1
+    fi
+
+    # Change back to the original directory
+    cd - >/dev/null 2>&1
 
     logInformation "Compressed contents of '$source' into '$destination'."
 }
@@ -433,6 +466,23 @@ assertVersionEquals() {
     # Log a message
     logInformation "Asserted that version '$version' equals version '$expected'."
 }
+
+# Asserts that Command Line Tools are installed, logs an error message and throws an error if not
+# Usage: assertXcodeToolsInstalled
+assertXcodeToolsInstalled() {
+    # Check for Command Line Tools by querying the location of 'xcode-select'
+    xcode-select -p &> /dev/null
+
+    # Check the exit code of the previous command
+    if [ $? -ne 0 ]; then
+        logWarning "Xcode Command Line Tools are not installed."
+        logWarning "Please run 'xcode-select --install' to install them."
+        logError "Unable to find Xcode Command Line Tools."
+    else
+        logInformation "Xcode Command Line Tools are installed."
+    fi
+}
+
 
 # Logging
 
